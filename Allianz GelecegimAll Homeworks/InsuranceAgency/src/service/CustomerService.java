361fdb86d4d323a22bc6f8c9f477e167ca51b5ef
Yamac Bayin <yamacbayin@gmail.com>
2023-07-17 15:21:43 +0300

    Move project to correct folder

---
+++ Allianz GelecegimAll Homeworks/InsuranceAgency/src/service/CustomerService.java
@@ -0,0 +1,66 @@
+package service;
+
+import model.*;
+
+import java.util.ArrayList;
+
+public class CustomerService {
+    public Customer createCustomer(String name, CustomerTypeEnum customerTypeEnum) {
+        Customer customer = new Customer();
+        customer.setName(name);
+        customer.setCustomerTypeEnum(customerTypeEnum);
+
+        return customer;
+    }
+
+    public void addBankAccountToCustomer(Customer customer, BankAccount bankAccount) {
+        if (customer.getBankAccountList() != null) {
+            customer.getBankAccountList().add(bankAccount);
+        } else {
+            ArrayList<BankAccount> bankAccountList = new ArrayList<>();
+            bankAccountList.add(bankAccount);
+            customer.setBankAccountList(bankAccountList);
+        }
+    }
+
+    public void addInsuranceRequestToCustomer(Customer customer, InsuranceRequest insuranceRequest) {
+        if (customer.getInsuranceRequestList() != null) {
+            customer.getInsuranceRequestList().add(insuranceRequest);
+        } else {
+            ArrayList<InsuranceRequest> insuranceRequestList = new ArrayList<>();
+            insuranceRequestList.add(insuranceRequest);
+            customer.setInsuranceRequestList(insuranceRequestList);
+        }
+    }
+
+    public void addPolicyListToCustomer(Customer customer, Policy policy) {
+        if (customer.getPolicyList() != null) {
+            customer.getPolicyList().add(policy);
+        } else {
+            ArrayList<Policy> policyRequestList = new ArrayList<>();
+            policyRequestList.add(policy);
+            customer.setPolicyList(policyRequestList);
+        }
+    }
+
+    public void addPaymentMovementCustomer(Customer customer, PaymentMovement paymentMovement) {
+        if (customer.getPaymentMovementList() != null) {
+            customer.getPaymentMovementList().add(paymentMovement);
+        } else {
+            ArrayList<PaymentMovement> paymentMovementList = new ArrayList<>();
+            paymentMovementList.add(paymentMovement);
+            customer.setPaymentMovementList(paymentMovementList);
+        }
+    }
+
+    public void addVehicleCustomer(Customer customer, Vehicle vehicle) {
+        if (customer.getVehicleList() != null) {
+            customer.getVehicleList().add(vehicle);
+        } else {
+            ArrayList<Vehicle> vehicleList = new ArrayList<>();
+            vehicleList.add(vehicle);
+            customer.setVehicleList(vehicleList);
+        }
+    }
+
+}
